0d101790afcda741a3b4211c42fb09ac03b41ef8
michaudrd <Renee.Michaud@edu.sait.ca>
2024-09-18 13:35:40 -0600

    Edited root page and added week-2 folder and student-info component

--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
-
+import Link from "next/link";
 
 export default function Home() {
-  return (
-    <main>
 
+  return (
+    <main className={"p-10"}>
+      <h1 className="text-3xl font-bold mb-3">CPRG 306: Web Development 2 - Assignments</h1>
+      <Link className={"text-lg hover:underline hover:text-green-500"} href="./week-2/">Week 2 Assignment</Link>
     </main>
   );
 }
---
+++ app/week-2/student-info.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+
+
+    return (
+        <main>
+            <h1>Renee Michaud</h1>
+            <Link href="https://github.com/michaudrd/cprg306-assignments">https://github.com/michaudrd/cprg306-assignments</Link>
+        </main>
+    );
+}
