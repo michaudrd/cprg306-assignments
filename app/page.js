b2bbb094b463daa08fe0406248c50b983857438d
michaudrd <Renee.Michaud@edu.sait.ca>
2024-09-28 13:23:50 -0600

    Added week-3 folder, created Item, ItemList, and Page components, updated page title

--- app/page.js
+++ app/page.js
@@ -5,7 +5,8 @@ export default function Home() {
   return (
     <main className={"p-10"}>
       <h1 className="text-3xl font-bold mb-3">CPRG 306: Web Development 2 - Assignments</h1>
-      <Link className={"text-lg hover:underline hover:text-green-500"} href="./week-2/">Week 2 Assignment</Link>
+      <p><Link className={"text-lg hover:underline hover:text-green-500"} href="./week-2/">Week 2 Assignment</Link></p>
+      <p><Link className={"text-lg hover:underline hover:text-green-500"} href="./week-3/">Week 3 Assignment</Link></p>
     </main>
   );
 }
