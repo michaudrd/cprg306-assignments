adb933a5452f06fc5f1e773832fcd8f114b9a331
michaudrd <Renee.Michaud@edu.sait.ca>
2024-11-04 07:45:27 -0700

    Added week-8 folder and functionality for displaying meal ideas of each list item

--- app/page.js
+++ app/page.js
@@ -11,6 +11,7 @@ export default function Home() {
       <p><Link className={"text-lg hover:underline hover:text-green-500"} href="./week-5/">Week 5 Assignment</Link></p>
       <p><Link className={"text-lg hover:underline hover:text-green-500"} href="./week-6/">Week 6 Assignment</Link></p>
       <p><Link className={"text-lg hover:underline hover:text-green-500"} href="./week-7/">Week 7 Assignment</Link></p>
+      <p><Link className={"text-lg hover:underline hover:text-green-500"} href="./week-8/">Week 8 Assignment</Link></p>
     </main>
   );
 }
